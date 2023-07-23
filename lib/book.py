6f7058b8bd4137338a854d1b42e9d8b9849eb48f
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-07-23 18:08:14 -0400

    update

--- lib/book.py
+++ lib/book.py
@@ -3,13 +3,18 @@
 class Book:
     def __init__(self, title, page_count):
         self.title = title
-        self.page_count = page_count
+        self._page_count = page_count
 
-    def turn_page(self):
-        print("Flipping the page...wow, you read fast!")
-        
-    def set_page_count(self, value):
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
         if not isinstance(value, int):
             print("page_count must be an integer")
         else:
-            self.page_counter = value
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
